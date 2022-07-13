8dac8eb85dc2a44b3e308b4d518d248f3db01f9f
Lukáš Vokráčko <lukas@b2c2.com>
2022-07-13 09:12:48 +0200

    Add v1.0 models

---
+++ stocra/v1_0/models.py
@@ -0,0 +1,89 @@
+from decimal import Decimal
+from typing import Any, Dict, List, Optional
+
+from pydantic import BaseModel, root_validator, validator
+
+Address = str
+TransactionHash = str
+OutputIndex = int
+
+
+class Amount(BaseModel):
+    value: Decimal
+    currency_symbol: str
+
+    def __add__(self, other: Any) -> "Amount":
+        if isinstance(other, Amount):
+            if self.currency_symbol != other.currency_symbol:
+                raise ValueError(
+                    f"Amounts have different currencies: {self.currency_symbol} != {other.currency_symbol}"
+                )
+
+            return Amount(value=self.value + other.value, currency_symbol=self.currency_symbol)
+
+        raise ValueError(f"Cannot add Amount and {type(other)}")
+
+    def __eq__(self, other: Any) -> bool:
+        if not isinstance(other, Amount):
+            raise ValueError(f"Cannot compare Amount and {type(other)}")
+
+        if self.value != other.value:
+            return False
+
+        if self.currency_symbol != other.currency_symbol:
+            return False
+
+        return True
+
+
+class TransactionPointer(BaseModel):
+    transaction_hash: TransactionHash
+    output_index: OutputIndex
+
+
+class Input(BaseModel):
+    address: Optional[Address] = None
+    amount: Optional[Amount] = None
+    transaction_pointer: Optional[TransactionPointer] = None
+
+    @root_validator
+    def validate_address_or_pointer(cls, values: Dict) -> Dict:  # pylint: disable=no-self-argument
+        if not (values["address"] or values["transaction_pointer"]):
+            raise ValueError("Either address or transaction pointer must be specified")
+
+        return values
+
+
+class Output(BaseModel):
+    address: Address
+    amount: Amount
+
+
+class Transaction(BaseModel):
+    hash: TransactionHash
+    inputs: List[Input]
+    outputs: List[Output]
+    fee: Amount
+
+    @classmethod
+    @validator("fee")
+    def validate_fee(cls, value: Amount) -> Amount:
+        if value.value < 0:
+            raise ValueError(f"Fee cannot be negative! Got {value.value}")
+
+        return value
+
+
+class Block(BaseModel):
+    height: int
+    hash: str
+    timestamp_ms: int
+    transactions: List[str] = []
+
+    @validator("timestamp_ms")
+    def validate_timestamp_ms(cls, value: int) -> int:  # pylint: disable=no-self-argument
+        # I don't really care if this runs even after 2286
+        if len(str(value)) != 13:
+            raise ValueError("Timestamp must be in miliseconds")
+
+        return value
---
+++ tests/tests_v1_0/test_models.py
@@ -0,0 +1,59 @@
+from decimal import Decimal
+
+import pytest
+
+from stocra.v1_0.models import Amount, Transaction
+
+
+def test_amount_add() -> None:
+    first = Amount(value=Decimal("1"), currency_symbol="BTC")
+    second = Amount(value=Decimal("2"), currency_symbol="BTC")
+    assert first + second == Amount(value=Decimal("3"), currency_symbol="BTC")
+
+
+def test_amount_add_wrong_currency() -> None:
+    first = Amount(value=Decimal("1"), currency_symbol="BTC")
+    second = Amount(value=Decimal("2"), currency_symbol="ETH")
+    with pytest.raises(ValueError):
+        first + second  # pylint:disable=pointless-statement
+
+
+def test_amount_add_wrong_type() -> None:
+    first = Amount(value=Decimal("1"), currency_symbol="BTC")
+    with pytest.raises(ValueError):
+        first + Decimal("2")  # pylint:disable=expression-not-assigned
+
+
+@pytest.mark.parametrize(
+    "first, second, expected_result",
+    [
+        (
+            Amount(value=Decimal("1"), currency_symbol="BTC"),
+            Amount(value=Decimal("1"), currency_symbol="BTC"),
+            True,
+        ),
+        (
+            Amount(value=Decimal("1"), currency_symbol="BTC"),
+            Amount(value=Decimal("2"), currency_symbol="BTC"),
+            False,
+        ),
+        (
+            Amount(value=Decimal("1"), currency_symbol="BTC"),
+            Amount(value=Decimal("1"), currency_symbol="ETH"),
+            False,
+        ),
+    ],
+)
+def test_amount_equal(first: Amount, second: Amount, expected_result: bool) -> None:
+    result = first == second
+    assert result is expected_result
+
+
+def test_amount_equal_wrong_type() -> None:
+    with pytest.raises(ValueError):
+        Amount(value=Decimal("1"), currency_symbol="BTC") == Decimal("1")  # pylint:disable=expression-not-assigned
+
+
+def test_negative_fee() -> None:
+    with pytest.raises(ValueError):
+        Transaction(fee=Amount(value=Decimal("-1"), currency_symbol="BTC"))
